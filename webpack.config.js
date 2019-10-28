2065006ec23225841fb63e8edebdc7a1bff8b541
Artem <tmailbox21@gmail.com>
2019-10-28 16:48:29 +0300

    Added SCSS plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
     entry: {
         app:'./src/index.js'
@@ -9,7 +9,39 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         publicPath: '/dist'
     },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader"
+            ]
+
+        },{
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {sourceMap: true}
+                }, {
+                    loader: 'sass-loader',
+                    options: {sourceMap: true}
+                }
+            ]
+
+        }]
+    },
+
     devServer: {
-        overlay: true
-    }
-}
+        overlay: true,
+        port: 8080
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: '[name].css',
+        })
+      ]
+};
